38dc285d623cb51fa3f9686693f0c85f5ce4e085
jlehrecke <jacklehrecke@gmail.com>
2014-11-24 19:43:49 +0100

    access file

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 //router.get('/', function(req, res) {
 //  res.render('index', { title: 'Express' });
 //});
 
-router.get('/users/:id', function(req, res){
-	console.log(req.query);
-	res.send(req.params.id, 200)
+router.get('/:id', function(req, res){
+	console.log("i am" + req.params.id);
+	fs.exists(req.params.id, function(exists) {
+	  if (exists) {
+	    fs.stat(req.params.id, function(error, stats) {
+	      fs.open(req.params.id, "r", function(error, fd) {
+	        var buffer = new Buffer(stats.size);
+	 
+	        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+	          var data = buffer.toString("utf8", 0, buffer.length);
+	 
+	          console.log(data);
+	          fs.close(fd);
+	        });
+	      });
+	    });
+	  }
+	});
+	res.render('index', {
+		title: 'blah',
+		age: req.params.id
+	})
 })
 
 module.exports = router;
